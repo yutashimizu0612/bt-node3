70a7b9895c08d50a38ea048ca0459f57aa88bc63
yutashimizu0612 <s08388yskh@gmail.com>
2020-09-28 23:40:13 +0900

    各クイズページの表示

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -1,16 +1,28 @@
 const fetch = require('node-fetch');
-const { quizzes } = require('../app');
+const app = require('../app');
 const { Quiz } = require('../models/Quiz');
 
+const url = 'https://opentdb.com/api.php?amount=';
+const AMOUNT = '10';
+
 exports.getQuizData = async () => {
-  const url = 'https://opentdb.com/api.php?amount=';
-  const AMOUNT = '10';
   try {
     const response = await fetch(url + AMOUNT);
     const data = await response.json();
-    quizzes = new Quiz(...data.results);
-    return quizzes;
+    app.quizzes = new Quiz(...data.results);
+    return app.quizzes;
   } catch (error) {
     console.log(error);
   }
+};
+
+exports.showQuizPage = (req, res) => {
+  const currentId = req.params.quizId;
+  const currentQuiz = app.quizzes['quiz' + currentId];
+  console.log('currentQuiz', currentQuiz);
+  res.render('pages/quiz', {
+    id: currentId,
+    next: currentId === AMOUNT ? 'result' : parseInt(currentId) + 1,
+    quiz: currentQuiz,
+  });
 };
--- routes/quiz.js
+++ routes/quiz.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const quizRouter = express.Router();
+const { showQuizPage } = require('../controllers/quiz');
 
-quizRouter.get('/', async(req, res) => {
-  res.render('pages/quiz');
+quizRouter.get('/result', async (req, res) => {
+  res.render('pages/result');
 });
+
+quizRouter.get('/:quizId', async (req, res) => showQuizPage(req, res));
 
 module.exports = quizRouter;
