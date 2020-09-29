374aebed25d034509588eb7f90ff1521eadb48b3
yutashimizu0612 <s08388yskh@gmail.com>
2020-09-29 09:04:54 +0900

    コメント追記

--- app.js
+++ app.js
@@ -9,13 +9,14 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
 
 const quizRouter = require('./routes/quiz');
-
 app.use('/quiz', quizRouter);
 
+// トップページ
 app.get('/', (req, res) => {
   res.render('pages/index');
 });
 
+// クイズ開始
 app.post('/', async (req, res) => {
   quizzes = await getQuizData();
   res.redirect('/quiz/1');
