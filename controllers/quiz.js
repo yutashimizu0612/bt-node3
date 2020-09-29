f40a2998c4c1280280fca499ec5289d1ffb7e5f5
yutashimizu0612 <s08388yskh@gmail.com>
2020-09-29 11:29:36 +0900

    クイズ正誤チェックと次ページへの遷移

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -5,6 +5,20 @@ const { Quiz } = require('../models/Quiz');
 const url = 'https://opentdb.com/api.php?amount=';
 const AMOUNT = '10';
 
+// 正誤判定後、次のクイズ画面へ遷移 // 【TODO】遷移もさせるなら関数名を変更したい
+const checkQuizAnswer = (selectedAnswer, correctAnswer, next, res) => {
+  if (selectedAnswer === correctAnswer) {
+    const currentCounter = localStorage.getItem('counter');
+    if (!currentCounter) {
+      localStorage.setItem('counter', '1');
+    } else {
+      const newCounter = parseInt(currentCounter) + 1;
+      localStorage.setItem('counter', newCounter.toString());
+    }
+  }
+  res.redirect('/quiz/' + next);
+};
+
 exports.getQuizData = async () => {
   try {
     const response = await fetch(url + AMOUNT);
@@ -24,5 +38,7 @@ exports.showQuizPage = (req, res) => {
     id: currentId,
     next: currentId === AMOUNT ? 'result' : parseInt(currentId) + 1,
     quiz: currentQuiz,
+    checkQuizAnswer,
+    res,
   });
 };
