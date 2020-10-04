da6b2475157d7db9de27062406b58adae5e3e686
yutashimizu0612 <s08388yskh@gmail.com>
2020-10-04 10:18:32 +0900

    DOM操作系の処理を削除

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -1,19 +1,12 @@
 const fetch = require('node-fetch');
 const app = require('../app');
 const { Quiz } = require('../models/Quiz');
-const jsdom = require('jsdom');
-const got = require('got');
 
 const url = 'https://opentdb.com/api.php?amount=';
 const AMOUNT = '10';
 
-const { JSDOM } = jsdom;
-
 exports.getQuizData = async () => {
   try {
-    // '/'のDOMを取得し、ロード中の表示に変更
-    const response = await got('http://localhost:3000');
-    const { document } = new JSDOM(response.body.toString()).window;
     document.getElementById('title').textContent = '取得中';
     document.getElementById('description').textContent = '少々お待ちください';
     // クイズデータを取得
