a64f67435ba3ea832856b0c976c41dc6c7838991
yutashimizu0612 <s08388yskh@gmail.com>
2020-10-05 21:14:25 +0900

    '/quiz'でquizデータ取得処理を実行

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const quizRouter = express.Router();
-const { showQuizPage } = require('../controllers/quiz');
+const { getQuizData, showQuizPage } = require('../controllers/quiz');
+
+// クイズデータを取得
+quizRouter.get('/', async (req, res) => {
+  const response = await getQuizData();
+  res.header('Content-Type', 'application/json; charset=utf-8');
+  res.send(response);
+});
 
 // 結果表示
 quizRouter.get('/result', async (req, res) => {
